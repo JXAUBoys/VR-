d6dca1098a3308bea872343486a458d81f37f3ca
JXAUBoys <43700012+JXAUBoys@users.noreply.github.com>
2018-10-03 11:46:06 +0800

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,14 +1,31 @@
-﻿#include <stdio.h>
+#include <stdio.h>
 int main()
 {
 	int n = 865751897;
-	for (int i = n-1; i >= 1; i--)
+	int factor = 2;
+	int lastfactor = 1;
+	while (n > 1)
 	{
-		if (n%i == 0)
+		if (n%factor == 0)
 		{
-			printf("最大质因数:%d\n", i);
-			break;
+			lastfactor = factor;
+			n = n / factor;
+			while (n % factor == 0)
+			{
+				n = n / factor;
+			}	
 		}
+		factor++;
 	}
-	return 0;
+	printf("%d\n", lastfactor);
 }
+/*
+对于给定的n, 使factor = 2, 3, 4, 5, 6...,对于每个factor, 当factor能被n完全整除时, 就到下一个factor.
+可以预见, 所有被整除的factor都是质因数,
+当所有小的因数都被整除时, n将会变为1
+如n为20, factor为2时, 20 % 2 = 0, n = n / 2, n变为10,
+return factor为10,
+10 % 2 = 0, n = n / 2, n变为5, (整除时将某一个因数整除完)
+然后下一个factor3, 4, 5, n % 5 = 0,
+return factor = 5, n变为1, 跳出循环
+*/
